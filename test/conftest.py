c3075448bb686b970b1a7e97827c89cb74bb8980
Cory Frontin <cory.frontin@nrel.gov>
2024-10-09 16:15:12 -0600

    black reformat

--- test/conftest.py
+++ test/conftest.py
@@ -3,10 +3,10 @@ import os
 import shutil
 import pytest
 
+
 def pytest_sessionfinish(session, exitstatus):
     # Cleanup code after tests
-    pytest_out_dir_pattern = 'pytest*_out'
+    pytest_out_dir_pattern = "pytest*_out"
     for pytest_out_dir in glob.glob(pytest_out_dir_pattern):
         if os.path.exists(pytest_out_dir):
             shutil.rmtree(pytest_out_dir)
-
