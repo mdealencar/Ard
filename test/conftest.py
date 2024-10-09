31c5a64ed765861274a61ab2f22b0a62db49712f
Cory Frontin <cory.frontin@nrel.gov>
2024-10-09 16:10:16 -0600

    added testing hooks to clear out pytest results

---
+++ test/conftest.py
@@ -0,0 +1,12 @@
+import glob
+import os
+import shutil
+import pytest
+
+def pytest_sessionfinish(session, exitstatus):
+    # Cleanup code after tests
+    pytest_out_dir_pattern = 'pytest*_out'
+    for pytest_out_dir in glob.glob(pytest_out_dir_pattern):
+        if os.path.exists(pytest_out_dir):
+            shutil.rmtree(pytest_out_dir)
+
