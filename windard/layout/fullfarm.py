6c3fb7d1e9c79d8ec1697d0f3d074bcc89882104
Cory Frontin <cory.frontin@nrel.gov>
2024-10-15 15:00:35 -0600

    added layback to full farm geometry

--- windard/layout/fullfarm.py
+++ windard/layout/fullfarm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import openmdao.api as om
 
+from shapely import length
 import shapely.geometry as sg
 
 import windard.layout.templates as templates
@@ -28,6 +29,15 @@ class FullFarmLanduse(templates.LanduseTemplate):
             desc="turbine location in y-direction",
         )
 
+        # add the layback output
+        self.add_output(
+            "area_layback",
+            0.0,
+            units="km**2",
+            desc="area of the farm geometry (plus layback)",
+        )
+
+
     def compute(self, inputs, outputs):
 
         # extract the points from the inputs
@@ -41,5 +51,10 @@ class FullFarmLanduse(templates.LanduseTemplate):
         # create a multi-point object
         mp = sg.MultiPoint(points)
 
+        # create a laybacked geometry
+        D_rotor = self.modeling_options["turbine"]["geometry"]["diameter_rotor"]
+        lengthscale_layback = float(inputs["distance_layback_diameters"] * D_rotor)
+
         # area tight is equal to the convex hull area for the points in sq. km.
         outputs["area_tight"] = mp.convex_hull.area / 1000**2
+        outputs["area_layback"]  = mp.convex_hull.buffer(lengthscale_layback).area / 1000**2
