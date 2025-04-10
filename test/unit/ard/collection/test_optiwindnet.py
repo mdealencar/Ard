135379dcbf426a1c71862848ecbac196d199462a
Jared Thomas <jaredthomas68@gmail.com>
2025-04-10 14:19:24 -0600

    have windows use the linux npz file

--- test/unit/ard/collection/test_optiwindnet.py
+++ test/unit/ard/collection/test_optiwindnet.py
@@ -218,7 +218,7 @@ class TestOptiWindNetCollection:
             # validate data against pyrite file
             ard.test_utils.pyrite_validator(
                 validation_data,
-                Path(__file__).parent / "test_optiwindnet_pyrite_macos.npz",
+                Path(__file__).parent / "test_optiwindnet_pyrite_linux.npz",
                 rtol_val=5e-3,
                 # rewrite=True,  # uncomment to write new pyrite file
             )
